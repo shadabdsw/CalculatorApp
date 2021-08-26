ee33afc6c7ff393cbb437e9343a0897af7b532b9
shadabdsw <shadab1996@gmail.com>
2021-08-27 04:48:03 +0530

    Added Dark Mode

--- src/main/java/com/falcon/Evaluate.java
+++ src/main/java/com/falcon/Evaluate.java
@@ -3,19 +3,19 @@ package com.falcon;
 import javax.script.*;
 
 public class Evaluate {
+
     public String evalExpression(String t) {
         ScriptEngineManager mgr = new ScriptEngineManager();
         ScriptEngine engine = mgr.getEngineByName("JavaScript");
-        try {
-            System.out.println(engine.eval(t));
-        } catch (ScriptException e) {
-            e.printStackTrace();
-        }
+
         try {
             return engine.eval(t).toString();
         } catch (ScriptException e) {
             e.printStackTrace();
         }
+
         return "Error Occured";
     }
 }
+
+// This function uses JavaScript engine to get the final result of an expression.
